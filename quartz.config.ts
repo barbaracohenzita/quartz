851c484c4714a6487f8b14e2f7d491d1a8393d7d
barbaracohenzita <fragolahomes@gmail.com>
2025-08-06 20:58:26 -0300

    Create quartz.config.ts

---
+++ quartz.config.ts
@@ -0,0 +1,36 @@
+const config: QuartzConfig = {
+  configuration: {
+    pageTitle: "Quartz 4",
+    // Change to your desired site title
+    ...
+    baseUrl: "quartz.jzhao.xyz",
+    // Change to your site URL without https://.
+    // This is your own domain,
+    // or "<github-user-name>.github.io/<repository-name>" when using GitHub Pages.
+    // See below for details
+    ...
+    defaultDateType: "modified",
+    // Change to tell Quartz what date to display on notes
+    // Valid options:
+    // "created", use when the note was created.
+    // "modified", use when the note was last modified.
+    // "published", use when the note was published.
+    // See Quartz docs for details.
+    ...
+    }
+  }
+  plugins: {
+    transformers: [
+      ...
+      Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
+      // Sets how Quartz should resolve links between notes.
+      // Should match the settings you use in Obsidian.
+      // Valid options:
+      // "shortest"
+      // "relative"
+      // "absolute"
+      ...
+    ]
+    ...
+  }
+}
